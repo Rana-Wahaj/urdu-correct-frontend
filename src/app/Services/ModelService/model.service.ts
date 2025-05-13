361b2a40f41e4ac174c5d2672180424e3a604a56
Rana Wahaj Ahmed <122843122+Rana-Wahaj@users.noreply.github.com>
2025-05-14 00:47:53 +0500

    Update model.service.ts

--- src/app/Services/ModelService/model.service.ts
+++ src/app/Services/ModelService/model.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ModelService {
-  private baseApiUrl = 'https://03b5-182-190-201-60.ngrok-free.app'; // Base URL for your FastAPI backend
-
+  // private baseApiUrl = 'https://03b5-182-190-201-60.ngrok-free.app'; // Base URL for your FastAPI backend
+  private baseApiUrl = 'https://0a31-2400-adc1-180-a600-dde3-328d-b1b0-3300.ngrok-free.app';
   constructor(private _httpClient: HttpClient) {}
 
   correctText(inputText: string, endpoint: string): Observable<any> {
